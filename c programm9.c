a22862a76f1ae41e5083129fbeadf9dfb7b43b4b
111-BOBBY <126140691+111-BOBBY@users.noreply.github.com>
2023-02-23 11:33:23 +0530

    Add files via upload

---
+++ c programm9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int val1,sqr;
+    printf("enter the no :",val1);
+    scanf("%d",&val1);
+    sqr=val1*val1;
+    printf("this ans is %d:",sqr);
+    
+}
