e20bb50eba445ba7b5961f1819a45281dfca3988
111-BOBBY <126140691+111-BOBBY@users.noreply.github.com>
2023-02-23 11:32:50 +0530

    Add files via upload

---
+++ c programm.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int val1,val2,product;
+    printf("enter a no",val1);
+    scanf("%d",&val1);
+    printf("enter a no",val2);
+    scanf("%d",&val2);
+    product=val1*val2;
+    printf("product of this %d:",product);
+}
